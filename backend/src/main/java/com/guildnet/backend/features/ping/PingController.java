ac9b9b6b0e9d31dffdb152e7d7ceee0731216bcb
AlvaroGomezNerpio <alvarogomeznerpio@gmail.com>
2025-06-09 20:34:03 +0200

    fix application.properties

---
+++ backend/src/main/java/com/guildnet/backend/features/ping/PingController.java
@@ -0,0 +1,13 @@
+package com.guildnet.backend.features.ping;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
